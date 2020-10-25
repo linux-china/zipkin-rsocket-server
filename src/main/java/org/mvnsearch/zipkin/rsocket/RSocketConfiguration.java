bdfb04c208c087f0af732a3dae1cac39e34001fc
linux_china <libing.chen@gmail.com>
2020-10-24 18:16:56 -0700

    adjusted to use RSocketServer

--- src/main/java/org/mvnsearch/zipkin/rsocket/RSocketConfiguration.java
+++ src/main/java/org/mvnsearch/zipkin/rsocket/RSocketConfiguration.java
@@ -1,6 +1,6 @@
 package org.mvnsearch.zipkin.rsocket;
 
-import io.rsocket.RSocketFactory;
+import io.rsocket.core.RSocketServer;
 import io.rsocket.transport.netty.server.CloseableChannel;
 import io.rsocket.transport.netty.server.TcpServerTransport;
 import org.springframework.context.annotation.Bean;
@@ -16,10 +16,8 @@ public class RSocketConfiguration {
 
     @Bean(destroyMethod = "dispose")
     public CloseableChannel rsocketServer(ZipkinRSocketAcceptor acceptor) {
-        return RSocketFactory.receive()
-                .acceptor(acceptor)
-                .transport(TcpServerTransport.create("0.0.0.0", 42252))
-                .start()
+        return RSocketServer.create(acceptor)
+                .bind(TcpServerTransport.create("0.0.0.0", 42252))
                 .block();
     }
 }
